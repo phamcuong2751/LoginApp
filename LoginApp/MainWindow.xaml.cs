23cb6e281b6364e1c262eb3d434666784003c8f8
unknown <phamcuong2751@gmai.com>
2020-05-20 10:05:18 +0700

    first commit

---
+++ LoginApp/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Data { get; set; } = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var DataUN = userName.Text;
+            var DataPW = password.Password;
+            if (DataUN.Length == 0 || DataPW.Length == 0)
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu sai cú pháp, vui lòng kiểm tra lại!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if(DataUN == "admin" && DataPW == "qwe3@1")
+            {
+                var nextPage = new DashboardWindow();
+                nextPage.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Chúc mừng, bạn đã đăng nhập thành công!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var nextPage = new ResetPassword();
+            nextPage.ShowDialog();
+        }
+    }
+}
---
+++ LoginApp/ResetPassword.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginApp
+{
+    /// <summary>
+    /// Interaction logic for ResetPassword.xaml
+    /// </summary>
+    public partial class ResetPassword : Window
+    {
+        public ResetPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetPasswordClick(object sender, RoutedEventArgs e)
+        {
+            var oldpass = "qwe3@1";
+            if (oldPassword.Password.Length == 0 || confirmPass.Password.Length == 0 || newPassword.Password.Length == 0)
+            {
+                MessageBox.Show("Lỗi cú pháp, ô đăng nhập không được để trống!", "Lỗi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (oldPassword.Password != oldpass)
+            {
+                MessageBox.Show("Mật khẩu cũ chưa đúng, vui lòng kiểm tra lại!", "Lỗi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (confirmPass.Password != newPassword.Password)
+            {
+                MessageBox.Show("Xác nhận mật khẩu mới thất bại, vui lòng kiểm tra lại!", "Lỗi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {               
+                MessageBox.Show("Cập nhật mật khẩu thành công!", "Chúc Mừng!", MessageBoxButton.OK, MessageBoxImage.Information);
+                oldPassword.Clear();
+                newPassword.Clear();
+                confirmPass.Clear();
+            }
+        }
+    }
+}
